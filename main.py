d9406d2c68f1aed54ca85864372866047c731d29
Sandesh0202 <sandeshpargaonkar7@gmail.com>
2023-06-22 23:29:55 +0530

    prepare Base Model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -12,5 +12,18 @@ try:
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+   logger.exception(e)
+   raise e
+     
+
+STAGE_NAME = "Prepare base Model"
+if __name__ == "__main__":
+   try:
+      logger.info(f"*************")
+      logger.info(f">>>>>>stage {STAGE_NAME} started <<<<<<<<")
+      obj = PrepareBaseModelTrainingPipeline()
+      obj.main()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n x=============x")
+   except Exception as e:
+      logger.exception(e)
+      raise e
